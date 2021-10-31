bedef9ee521722a17dd59e2bd810c256c9760b2c
trevcoe <88930059+trevcoe@users.noreply.github.com>
2021-10-31 19:04:05 -0400

    Add files via upload

---
+++ part-3/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+    return (
+        <div>
+            <person
+                name="Jake"
+                age={26}
+                hobbies ={["gaming, working out, playing guitar"]}
+            />
+            <person
+                name="Spif"
+                age={31}
+                hobbies ={["working, gaming, eating spifnach"]}
+            />
+            <person
+                name="Dibster"
+                age={24}
+                hobbies ={["eating, gaming, eating"]}
+            />
+        </div>
+    );
+}
